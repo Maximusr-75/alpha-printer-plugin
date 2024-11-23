00444dae1b91abc0b99ae28407888da3417b505f
Maximusr-75 <89749875+Maximusr-75@users.noreply.github.com>
2024-11-23 14:25:22 +0000

    PrinterPlug.java

--- src/android/com/PrinterPlug.java
+++ src/android/com/PrinterPlug.java
@@ -1,83 +1,65 @@
-package com.alphaprinter;
+package com.alphaplugin.printer;
 
 import org.apache.cordova.CordovaPlugin;
 import org.apache.cordova.CallbackContext;
 import org.json.JSONArray;
 import org.json.JSONException;
-import android.os.AsyncTask;
+import android.util.Log;
 import java.net.Socket;
 import java.io.PrintWriter;
 import java.io.OutputStreamWriter;
 
-public class PrinterPlugin extends CordovaPlugin {
+public class POSPrinterPlugin extends CordovaPlugin {
+    private static final String TAG = "POSPrinterPlugin";
+
     @Override
     public boolean execute(String action, JSONArray args, CallbackContext callbackContext) throws JSONException {
         if (action.equals("printData")) {
             String ip = args.getString(0);
-            String port = args.getString(1);
+            int port = args.getInt(1);
             String data = args.getString(2);
             this.printData(ip, port, data, callbackContext);
             return true;
         }
-        else if (action.equals("testPrinter")) {
+        else if (action.equals("testConnection")) {
             String ip = args.getString(0);
-            String port = args.getString(1);
-            this.testPrinter(ip, port, callbackContext);
+            int port = args.getInt(1);
+            this.testConnection(ip, port, callbackContext);
             return true;
         }
         return false;
     }
 
-    private void printData(final String ip, final String port, final String data, final CallbackContext callbackContext) {
-        new AsyncTask<Void, Void, Boolean>() {
-            @Override
-            protected Boolean doInBackground(Void... params) {
+    private void printData(final String ip, final int port, final String data, final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
                 try {
-                    Socket socket = new Socket(ip, Integer.parseInt(port));
+                    Socket socket = new Socket(ip, port);
                     OutputStreamWriter outputStream = new OutputStreamWriter(socket.getOutputStream());
                     outputStream.write(data);
                     outputStream.flush();
                     socket.close();
-                    return true;
-                } catch (Exception e) {
-                    e.printStackTrace();
-                    return false;
-                }
-            }
-
-            @Override
-            protected void onPostExecute(Boolean result) {
-                if (result) {
                     callbackContext.success("Print successful");
-                } else {
-                    callbackContext.error("Print failed");
+                } catch (Exception e) {
+                    Log.e(TAG, "Print error", e);
+                    callbackContext.error("Print failed: " + e.getMessage());
                 }
             }
-        }.execute();
+        });
     }
 
-    private void testPrinter(final String ip, final String port, final CallbackContext callbackContext) {
-        new AsyncTask<Void, Void, Boolean>() {
-            @Override
-            protected Boolean doInBackground(Void... params) {
+    private void testConnection(final String ip, final int port, final CallbackContext callbackContext) {
+        cordova.getThreadPool().execute(new Runnable() {
+            public void run() {
                 try {
-                    Socket socket = new Socket(ip, Integer.parseInt(port));
+                    Socket socket = new Socket(ip, port);
                     socket.close();
-                    return true;
+                    callbackContext.success("Connection successful");
                 } catch (Exception e) {
-                    e.printStackTrace();
-                    return false;
+                    Log.e(TAG, "Connection test failed", e);
+                    callbackContext.error("Connection failed: " + e.getMessage());
                 }
             }
-
-            @Override
-            protected void onPostExecute(Boolean result) {
-                if (result) {
-                    callbackContext.success("Printer connected");
-                } else {
-                    callbackContext.error("Printer connection failed");
-                }
-            }
-        }.execute();
+        });
     }
 }
