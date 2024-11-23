6dbd7b51231ec9418107e08defb9c99d8c26a22e
Maximusr-75 <89749875+Maximusr-75@users.noreply.github.com>
2024-11-23 14:29:40 +0000

    printer.js

--- www/printer.js
+++ www/printer.js
@@ -1,13 +1,35 @@
 var exec = require('cordova/exec');
 
-var PrinterPlugin = {
-    printData: function(ip, port, data, success, error) {
-        exec(success, error, 'PrinterPlugin', 'printData', [ip, port, data]);
+var POSPrinter = {
+    printData: function(ip, port, data) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, 'POSPrinterPlugin', 'printData', [ip, port, data]);
+        });
     },
-    
-    testPrinter: function(ip, port, success, error) {
-        exec(success, error, 'PrinterPlugin', 'testPrinter', [ip, port]);
+
+    testConnection: function(ip, port) {
+        return new Promise(function(resolve, reject) {
+            exec(resolve, reject, 'POSPrinterPlugin', 'testConnection', [ip, port]);
+        });
+    },
+
+    // Helper function to format TSPL commands
+    formatTSPL: function(content) {
+        var command = "SIZE 80 mm, 60 mm\n" +
+                     "GAP 3 mm, 0 mm\n" +
+                     "DIRECTION 1\n" +
+                     "CLS\n";
+        
+        var yPos = 10;
+        content.split('\n').forEach(function(line) {
+            if(line.trim()) {
+                command += 'TEXT 10,' + yPos + ',"3",0,1,1,"' + line.trim() + '"\n';
+                yPos += 25;
+            }
+        });
+        
+        return command + "PRINT 1\n";
     }
 };
 
-module.exports = PrinterPlugin;
+module.exports = POSPrinter;
